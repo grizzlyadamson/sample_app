a570f5d966bbd5310daa6709c257cd4744b177ab
Grizzly <grizzlyadamson@gmail.com>
2013-03-25 18:24:14 -0500

    Fixed.  Start at 9.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only:  [:edit, :update]
+  before_filter :signed_in_user, only:  [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
   
   def index
+    @users = User.all
   end
 
   def show
@@ -42,7 +43,10 @@ class UsersController < ApplicationController
   private
 
     def signed_in_user
-        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user
