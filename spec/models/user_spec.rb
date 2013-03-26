e71c80f038145b2e9c98ad06d1b7e7d93975aedb
Grizzly <grizzlyadamson@gmail.com>
2013-03-26 18:49:18 -0500

    Up to 10.23.  Posts are viewable.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -146,7 +146,7 @@ describe User do
     it "should destroy associated microposts" do
       microposts = @user.microposts.dup
       @user.destroy
-      microposts.should_not be empty
+      microposts.should_not be_empty
       microposts.each do |micropost|
         Micropost.find_by_id(micropost.id).should be_nil
       end
